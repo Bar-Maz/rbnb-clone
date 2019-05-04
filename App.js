17d7c9652e7fd0e2e013e5eb2ab203daeb411f57
Bartosz Maziarka <bahtek.maziarka@gmail.com>
2019-05-04 23:49:14 +0200

    Navigator styling

--- App.js
+++ App.js
@@ -3,9 +3,23 @@ import {createBottomTabNavigator, createAppContainer} from "react-navigation";
 import {ExploreScreen, InboxScreen, ProfileScreen, SavedScreen, TripsScreen} from './screens';
 
 export default createAppContainer(createBottomTabNavigator({
-  Explore:  ExploreScreen,
+  Explore: ExploreScreen,
   Saved: SavedScreen,
   Trips: TripsScreen,
   Inbox: InboxScreen,
   Profile: ProfileScreen,
+
+}, {
+  tabBarOptions: {
+    activeTintColor: 'red',
+    inactiveTintColor: 'grey',
+    style: {
+      backgroundColor: 'white',
+      borderTopWidth: 0,
+      shadowStyle: {width: 5, height: 3},
+      shadowColor: 'grey',
+      shadowOpacity: '0.7',
+      elevation: 5,
+    }
+  }
 }));
