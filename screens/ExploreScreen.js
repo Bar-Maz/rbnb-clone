0045f41e77354539c6df73f7371fc65c2de9b2e2
Bartosz Maziarka <bahtek.maziarka@gmail.com>
2019-05-05 01:14:19 +0200

    Searchbar placement and styling

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,38 +1,70 @@
-import React, { Component } from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import React, {Component} from 'react';
+import {View, StatusBar, Platform, SafeAreaView, TextInput} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 class ExploreScreen extends Component {
-    render() {
-        return (
-            <View style={styles.container}>
-                <Text>ExploreScreen</Text>
-            </View>
-        );
+  componentWillMount(): void {
+    this.startHeaderHeight = 80;
+    if(Platform.OS === 'android'){
+      this.startHeaderHeight = 100 + StatusBar.currentHeight;
     }
+  }
+
+  render() {
+    return (
+      <SafeAreaView style={{flex: 1}}>
+        <View style={{flex: 1}}>
+          <View style={{
+            height: this.startHeaderHeight,
+            backgroundColor: 'white',
+            borderBottomColor: '#ddd',
+            borderBottomWidth: 1
+          }}>
+            <View style={{
+              flexDirection: 'row',
+              backgroundColor: 'white',
+              marginHorizontal: 20,
+              paddingHorizontal: 10,
+              shadowOffset: {width: 0, height: 0},
+              shadowColor: 'grey',
+              shadowOpacity: '0.4',
+              elevation: 1,
+              alignItems: 'center',
+              marginTop: Platform.OS === 'android'? 30: null,
+            }}>
+              <FontAwesome5 name={'search'} size={20}
+              style={{marginRight: 10}}/>
+              <TextInput placeholder="Try Rzeszów"
+                         placeholderTextColor="grey"
+                         UnderlineColorAndroid = "transparent"
+                         style={{
+                           flex: 1,
+                           fontWeight: '700',
+                           backgroundColor: 'white',
+                         }}/>
+            </View>
+          </View>
+        </View>
+      </SafeAreaView>
+    );
+  }
 }
+
 ExploreScreen = createStackNavigator({
-   main: ExploreScreen
-    },
+    main: ExploreScreen
+  },
   {
-      headerMode: 'none',
-      navigationOptions: {
-          headerVisible: false,
-          tabBarLabel: 'EXPLORE',
-          tabBarIcon: ({ tintColor }) => (
-            <FontAwesome5 name = "search"
-                  color = {tintColor}
-                  size = {24}
-                  solid/>
-          ),
-      }
+    headerMode: 'none',
+    navigationOptions: {
+      headerVisible: false,
+      tabBarLabel: 'EXPLORE',
+      tabBarIcon: ({tintColor}) => (
+        <FontAwesome5 name="search"
+                      color={tintColor}
+                      size={24}
+                      solid/>
+      ),
+    }
   });
 export default ExploreScreen;
-const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-    }
-});
