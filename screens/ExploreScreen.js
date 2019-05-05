728e562eaccdc782e1d2eebb10410982b4c01ac0
Bartosz Maziarka <bahtek.maziarka@gmail.com>
2019-05-05 23:32:17 +0200

    Added categories to the horizontal scroll

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -2,6 +2,7 @@ import React, {Component} from 'react';
 import {Text, View, StatusBar, Platform, SafeAreaView, TextInput, ScrollView} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
+import Category from "../components/Explore/Category";
 
 class ExploreScreen extends Component {
   componentWillMount(): void {
@@ -57,12 +58,12 @@ class ExploreScreen extends Component {
               }}>
                 What can we help you find?
               </Text>
-              <View style={{height: 120, marginTop: 15,}}>
+              <View style={{height: 140, marginTop: 15,}}>
                 <ScrollView
                   horizontal={true}>
-                  <Text style={{paddingHorizontal: 20}}>
-                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id faucibus nisl tincidunt eget nullam. Tellus in metus vulputate eu scelerisque felis imperdiet proin. Interdum posuere lorem ipsum dolor. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Ut sem nulla pharetra diam sit amet nisl suscipit. Id nibh tortor id aliquet lectus proin nibh nisl. Id eu nisl nunc mi ipsum. Venenatis tellus in metus vulputate. Cursus mattis molestie a iaculis. Urna et pharetra pharetra massa massa ultricies.
-                  </Text>
+                  <Category image={require('../assets/home.jpg')} title={'Home'}/>
+                  <Category image={require('../assets/bike.jpg')} title={'Experiences'}/>
+                  <Category image={require('../assets/restaurant.jpg')} title={'Restaurant'}/>
                 </ScrollView>
               </View>
             </View>
