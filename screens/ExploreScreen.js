39cd656c6305aebe0469cd2544de0ef032779e67
Bartosz Maziarka <bahtek.maziarka@gmail.com>
2019-05-05 22:47:56 +0200

    Horizontal scroll setup

--- screens/ExploreScreen.js
+++ screens/ExploreScreen.js
@@ -1,12 +1,12 @@
 import React, {Component} from 'react';
-import {View, StatusBar, Platform, SafeAreaView, TextInput} from 'react-native';
+import {Text, View, StatusBar, Platform, SafeAreaView, TextInput, ScrollView} from 'react-native';
 import {createStackNavigator} from 'react-navigation';
 import FontAwesome5 from "react-native-vector-icons/FontAwesome5";
 
 class ExploreScreen extends Component {
   componentWillMount(): void {
     this.startHeaderHeight = 80;
-    if(Platform.OS === 'android'){
+    if (Platform.OS === 'android') {
       this.startHeaderHeight = 100 + StatusBar.currentHeight;
     }
   }
@@ -31,13 +31,13 @@ class ExploreScreen extends Component {
               shadowOpacity: '0.4',
               elevation: 1,
               alignItems: 'center',
-              marginTop: Platform.OS === 'android'? 30: null,
+              marginTop: Platform.OS === 'android' ? 30 : null,
             }}>
               <FontAwesome5 name={'search'} size={20}
-              style={{marginRight: 10}}/>
+                            style={{marginRight: 10}}/>
               <TextInput placeholder="Try Rzeszów"
                          placeholderTextColor="grey"
-                         UnderlineColorAndroid = "transparent"
+                         UnderlineColorAndroid="transparent"
                          style={{
                            flex: 1,
                            fontWeight: '700',
@@ -45,6 +45,29 @@ class ExploreScreen extends Component {
                          }}/>
             </View>
           </View>
+          <ScrollView
+            scrollEventThrottle={16}>
+            <View style={{
+              flex: 1, backgroundColor: 'white',
+              paddingTop: 20
+            }}>
+              <Text style={{
+                fontSize: 24, fontWeight: '700',
+                paddingHorizontal: 20
+              }}>
+                What can we help you find?
+              </Text>
+              <View style={{height: 120, marginTop: 15,}}>
+                <ScrollView
+                  horizontal={true}>
+                  <Text style={{paddingHorizontal: 20}}>
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Id faucibus nisl tincidunt eget nullam. Tellus in metus vulputate eu scelerisque felis imperdiet proin. Interdum posuere lorem ipsum dolor. Turpis egestas pretium aenean pharetra magna ac placerat vestibulum lectus. Ut sem nulla pharetra diam sit amet nisl suscipit. Id nibh tortor id aliquet lectus proin nibh nisl. Id eu nisl nunc mi ipsum. Venenatis tellus in metus vulputate. Cursus mattis molestie a iaculis. Urna et pharetra pharetra massa massa ultricies.
+                  </Text>
+                </ScrollView>
+              </View>
+            </View>
+
+          </ScrollView>
         </View>
       </SafeAreaView>
     );
